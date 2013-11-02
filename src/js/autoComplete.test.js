e91c7e67b54bc152901e996a398a764a58033692
Lars Thorup <lars@zealake.com>
2013-11-02 15:34:30 -0700

    mocking global functions

--- src/js/autoComplete.test.js
+++ src/js/autoComplete.test.js
@@ -1,7 +1,7 @@
 /*jshint undef:false, strict:false*/ // Note: to avoid having to write QUnit.module, etc
 module('autoComplete', {
     setup: function () {
-        this.sandbox = sinon.sandbox.create();
+        this.sinon = sinon.sandbox.create();
         $('<input id="name" />').appendTo('#qunit-fixture');
         this.autoComplete = new AutoComplete('#name', {
             list: ['lars', 'matt', 'mike', 'nick', 'kin'],
@@ -9,21 +9,39 @@ module('autoComplete', {
         });
     },
     teardown: function () {
-        this.sandbox.restore();
+        this.sinon.restore();
     }
 });
 
 test('keypress', function () {
     // given
-    this.sandbox.stub(this.autoComplete, 'getMatch').returns('monique');
+    this.sinon.stub(this.autoComplete, 'getMatch').returns('monique');
     var keypressEvent = $.Event('keypress', {charCode: 109}); // Note: 'm'
-    this.sandbox.spy(keypressEvent, 'preventDefault');
+    this.sinon.spy(keypressEvent, 'preventDefault');
 
     // when
     $('#name').trigger(keypressEvent);
 
     // then
-    ok(this.autoComplete.getMatch.calledWith('m'), 'called getMatch("m")');
+    ok(this.autoComplete.getMatch.calledWith('m'), 'getMatch("m") called');
     ok(keypressEvent.preventDefault.called, 'event cancelled');
-    equal($('#name').val(), 'monique', 'value is set');
+    equal($('#name').val(), 'monique', 'value changed');
+});
+
+test('openPopup', function () {
+    // given
+    var popup;
+    this.sinon.stub(window, 'open', function () {
+        popup = {
+            focus: sinon.spy()
+        };
+        return popup;
+    });
+
+    // when
+    this.autoComplete.openPopup('zealake.com');
+
+    // then
+    ok(window.open.calledWith('zealake.com', '_blank', 'resizable'), 'window.open called');
+    ok(popup.focus.called, 'focus changed');
 });
